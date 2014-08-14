9dc87d7ebce4b8b768b9a3e98cc2cd649382dcb6
Henia Netzel <henia@henia.localdomain>
2014-08-14 11:48:52 +0200

    dodany pik2

--- res3.f90
+++ res3.f90
@@ -1,4 +1,4 @@
-program res3 !wywolanie programu: res <nazwa_pliku_z_residuami> <okres> [przedzial czasowy danych]
+program res3bl !wywolanie programu: res <nazwa_pliku_z_residuami> <okres> [przedzial czasowy danych]
 implicit none
 
 	character (len=30) :: plik
@@ -20,49 +20,33 @@ implicit none
 	real(kind=8) :: period,f
 	real(kind=8) :: ft,at,snt !trend
 	real(kind=8) :: fp,ap,snp !w pobliżu piku głównego
-!	real(kind=8) :: f2,a2,sn2 !efekt instrumentalny w 2
+	real(kind=8) :: f2,a2,sn2 !efekt instrumentalny w 2
 !	real(kind=8) :: f1,a1,sn1 !efekt instrumentalny w 1
 	real(kind=8) :: fp2,ap2,snp2 !w pobliżu 2f
 	real(kind=8) :: fx,ax,snx !w szukanym zakresie, szukana czestosc X
 	
 	real :: r ! rozdzielczosc
 	
-	logical :: czy_zo=.false., czy_trend=.false., czy_bl=.false., czy_mod=.false., czy_al=.false.
-
+	logical :: czy_zo=.false., czy_trend=.false., czy_bl=.false., czy_mod=.false., czy_al=.false., czy_p2=.false.
 
 
 
 call get_arg (plik,period,f,r)				!wczytywanie argumentow wywolania
 call get_file(fn,fs,n) 					!wczytywanie pliku res do tablicy
 
-!write (*,*) 'rozdzielczosc: ',r,1/r
-
-!fragment do testowania
-!open (60,file='sss.txt')
-!do i=1,n
-!	write (60,*) fs(1,i),fs(2,i),fs(3,i)
-!end do
-!close (60)
-!~~~~~~~~~~~~~~~~~~
-
-
-
 call trend	(fs,n,ft,r)		!max dla malych czestotliwosci f<0.003
-call pik_gl (bl)				!max w poblizu piku glownego - sprawdzanie czy efekt Blazki (odrzucenie gwiazdy do innej analizy)
+call pik2 (fs,n,f2,r)		!pik w okolicach 2
+call pik_gl (bl)			!max w poblizu piku glownego - sprawdzanie czy efekt Blazki (odrzucenie gwiazdy do innej analizy)
 call szukaj_px 				!max w szukanym zakresie
 
 !fragment do testowania
-open (50,file='ppp.txt')
-do i=1,n
-	write (50,*) fs(1,i),fs(2,i),fs(3,i)
-end do
-close (50)
+!open (50,file='ppp.txt')
+!do i=1,n
+!	write (50,*) fs(1,i),fs(2,i),fs(3,i)
+!end do
+!close (50)
 !~~~~~~~~~~~~~~~~~~
 	
-!write (line,'(a25,f11.7,6f20.16,a5)') plik, period,f1,sn1,f2,sn2,f3,sn3,flaga
-!write (*,*) line
-!call system('echo " '//line//' ">>residua.txt')
-
 deallocate(fs)
 close(fn)
 
@@ -70,21 +54,44 @@ call flagi(bl)
 
 CONTAINS
 
-subroutine aliasy (fs,n,freq,r)	!wszystkie czestosci w odleglosci [freq,freq+almax] sa zaznaczone jako niepewne (2.0) , nie robie tego dla gwiazd z Blazka
+subroutine pik2(f2,n,ft,r)
+implicit none
+
+	real(kind=8), dimension (:,:), allocatable :: fs
+	integer :: n !liczba wierszy
+	real(kind=8) :: f2
+	real :: r ! rozdzielczosc
+	real :: d=1.0027 ! doba
+	integer :: i,j
+			
+
+	call maximum(fs,n,f2,a2,dble(1.98),dble(2.02),k)
+	call sn(fs,n,f2,0.2,sn2)
+	if (snt.gt.4.0) then
+		czy_p2=.true.
+		do j=1,9
+			call aliasy_roczne(fs,n,f2+(j-1)*d,r,2.0)
+		end do 
+		call aliasy_dobowe (fs,n,f2,r)
+	end if
+
+end subroutine
+
+subroutine aliasy_roczne (fs,n,freq,r,s)	!wszystkie czestosci w odleglosci [freq,freq+almax] sa zaznaczone jako niepewne (2.0) 
 implicit none
 
 	real(kind=8), dimension (:,:), allocatable :: fs
 	integer :: n,i
 	real(kind=8) :: freq
-	real(kind=8),parameter :: almin=0.00274, almax=0.00278	!srednia odleglosc aliasów rocznych moze 0.00274 - 0.00278?
+	real(kind=8),parameter :: almin=0.00274, almax=0.00278	
 	real :: r !rozdzielczosc
+	real :: s !liczba wpisywana zamiast 1.0
 	logical :: czy_al
 	
 	
 	do i=1,n
 		if ((abs(fs(1,i)-freq).gt.0).and.(abs(fs(1,i)-freq).lt.(almax+r)).and.fs(3,i).eq.1.0) then
-			fs(3,i)=2.0
-		!	write (*,*) abs(fs(1,i)-freq)
+			fs(3,i)=s
 		end if
 	end do
 
@@ -97,13 +104,9 @@ subroutine szukaj_px
 
 	call maximum(fs,n,fx,ax,fmin,fmax,k)
 	call sn(fs,n,fx,0.2,snx)
-!	write (*,*) ' fx: ',fx,'snx: ',snx, 'px/p=',f/fx
 	if (snx.gt.4.0) then
-	!	write (*,*) 'JEST MOD!'
 		czy_mod=.true.
 		if (fs(3,k).eq.2.0) czy_al=.true.
-!	else
-!		write (*,*) 'nic'
 	end if
 
 end subroutine
@@ -120,32 +123,29 @@ implicit none
 	do
 		call maximum(fs,n,fp,ap,fmin,fmax,k)
 		call sn(fs,n,fp,0.2,snp)
-	!	write (*,*) 'fp=',fp,'snp=',snp
 		if (snp.gt.4.0) then
 			if (abs(fp-f).lt.r) then
-			write (*,*) fp,snp,'Zmiana okresu',1/(f-fp)
-				do i=1,n
-					if ((fs(1,i).gt.(fp-r)).and.(fs(1,i).lt.(fp+r))) fs(3,i)=0.0
-				end do
+			!write (*,*) fp,snp,'Zmiana okresu',1/(f-fp)
 				czy_zo=.true.
-				call zo	(fs,n,fp,r)
-				call aliasy (fs,n,fp,r)			
+				call aliasy_dobowe	(fs,n,fp,r)
+				call aliasy_roczne (fs,n,fp,r,3.0)			
 			else
 			!	write(*,*) fp,snp,'Blazko? Osobna analiza',1/(f-fp)
-				write (pom2,*) 1/(f-fp)
-				pom = trim(bl)
-				bl = trim(pom)//trim(pom2)
-				czy_bl=.true.
-				do i=1,n
-					if ((fs(1,i).gt.(fp-r)).and.(fs(1,i).lt.(fp+r))) fs(3,i)=0.0
-				end do
-				call aliasy (fs,n,fp,r) ! nie wiem czy to juz nie bedzie za duzo
-				call blazko (fs,n,fp,r)
+				!write (pom2,*) 1/(f-fp)
+				!pom = trim(bl)
+				!bl = trim(pom)//trim(pom2)
+				if (fs(3,k).ne.3.0) then
+					czy_bl=.true.
+					write (pom2,*) 1/(f-fp)
+					pom = trim(bl)
+					bl = trim(pom)//trim(pom2)
+				end if
+				call aliasy_roczne (fs,n,fp,r,3.0) 
+				call aliasy_dobowe (fs,n,fp,r)
 				fmax=max(f+2*abs(f-fp)+r,f+0.2)
 				fmin=min(f-2*abs(f-fp)-r,f-0.2)
 			end if
 		else
-	!		write (*,*) 'Brak wyraznego sygnalu w poblizu glownej czestosci'
 			exit
 		end if
 		
@@ -153,7 +153,7 @@ implicit none
 
 end subroutine
 
-subroutine blazko(fs,n,freq,r)
+subroutine aliasy_dobowe(fs,n,freq,r)
 
 	real(kind=8), dimension (:,:), allocatable :: fs
 	integer :: n,i,j
@@ -168,26 +168,6 @@ subroutine blazko(fs,n,freq,r)
 			end if
 		end do
 	end do
-
-end subroutine
-
-subroutine zo (fs,n,fp,r)
-implicit none
-
-	real(kind=8), dimension (:,:), allocatable :: fs
-	integer :: n !liczba wierszy
-	real(kind=8) :: fp
-	real :: r ! rozdzielczosc
-	real :: d=1.0027 ! doba gwiazdowa
-	integer :: i,j
-	
-	do j=1,9
-		do i=1,n
-			if((fs(1,i).ge.(fp+j*d-r)).and.(fs(1,i).le.(fp+j*d+r))) then
-				fs(3,i)=0.0
-			end if
-		end do
-	end do	
 
 end subroutine
 
@@ -220,6 +200,11 @@ implicit none
 		line=trim(pom)//" ZO "
 	end if
 	
+	if (czy_p2) then
+		pom=line
+		line=trim(pom)//" P2 "
+	end if
+	
 	if (czy_mod) then
 		pom=line
 		line=trim(pom)//" MOD "
@@ -235,17 +220,17 @@ implicit none
 	pom=line
 	line=trim(pom)//trim(bl)
 
-	write (*,*) trim(line)
+	!write (*,*) trim(line)
 	!write (*,*) trim(blazko)
 	
-	!if (czy_bl) call system('echo " '//trim(line)//' " >> res3-bl.txt')
+	call system('echo " '//trim(line)//' " >> rrc2.txt')
 
 end subroutine
 
 subroutine trend(fs,n,ft,r)
 implicit none
 
-	integer :: k,u=500
+	integer :: k
 	
 	real(kind=8), dimension (:,:), allocatable :: fs
 	integer :: n !liczba wierszy
@@ -253,75 +238,16 @@ implicit none
 	real :: r ! rozdzielczosc
 	real :: d=1.0027 ! doba
 	integer :: i,j
-	
-		
+			
 
 	call maximum(fs,n,ft,at,dble(0.0),dble(0.003),k)
 	call sn(fs,n,ft,0.2,snt)
-!	write (*,*) 'ft=',ft,'snt=',snt
-	!jesli trend istnieje to wywalanie z analizy pikow z okna spektralnego dla 0.0
-	!piki:
-	!0.0000227
-	!1.0027527
-	!2.0027346
-	!3.0080538
-	!4.0110110
-	!5.0110383
-	!6.0000049
-	!7.0191783
-	!7.9945178
 	if (snt.gt.4.0) then
 		czy_trend=.true.
 		do j=1,9
-			call aliasy(fs,n,ft+(j-1)*d,r)
+			call aliasy_roczne(fs,n,ft+(j-1)*d,r,2.0)
 		end do 
-	!	call aliasy (fs,n,dble(0.0000227),r)
-	!	call aliasy (fs,n,dble(1.0027527),r)
-	!	call aliasy (fs,n,dble(2.0027346),r)
-	!	call aliasy (fs,n,dble(3.0080538),r)
-	!	call aliasy (fs,n,dble(4.0110110),r)
-	!	call aliasy (fs,n,dble(5.0110383),r)
-	!	call aliasy (fs,n,dble(6.0000049),r)
-	!	call aliasy (fs,n,dble(7.0191783),r)
-	!	call aliasy (fs,n,dble(7.9945178),r)
-	!	write (*,*) 'jest trend'
-		do j=1,9
-			do i=1,n
-				if((fs(1,i).ge.(ft+(j-1)*d-r)).and.(fs(1,i).le.(ft+(j-1)*d+r))) then
-					fs(3,i)=0.0
-				end if
-			end do
-		end do
-!		do i=1,n
-!			if ((fs(1,i).gt.0.0000227-r).and.(fs(1,i).lt.0.0000227+r)) then
-!				fs(3,i)=0.0
-
-!			else if ((fs(1,i).gt.1.0027527-r).and.(fs(1,i).lt.1.0027527+r)) then
-!				fs(3,i)=0.0
-
-!			else if ((fs(1,i).gt.2.0027346-r).and.(fs(1,i).lt.2.0027346+r)) then
-!				fs(3,i)=0.0
-
-!			else if ((fs(1,i).gt.3.0080538-r).and.(fs(1,i).lt.3.0080538+r)) then
-!				fs(3,i)=0.0
-			
-!			else if ((fs(1,i).gt.4.0110110-r).and.(fs(1,i).lt.4.0110110+r)) then
-!				fs(3,i)=0.0
-
-!			else if ((fs(1,i).gt.5.0110383-r).and.(fs(1,i).lt.5.0110383+r)) then
-!				fs(3,i)=0.0
-
-!			else if ((fs(1,i).gt.6.0000049-r).and.(fs(1,i).lt.6.0000049+r)) then
-!				fs(3,i)=0.0
-
-!			else if ((fs(1,i).gt.7.0191783-r).and.(fs(1,i).lt.7.0191783+r)) then
-!				fs(3,i)=0.0
-
-!			else if ((fs(1,i).gt.7.9945178-r).and.(fs(1,i).lt.7.9945178+r)) then
-!				fs(3,i)=0.0
-
-!			end if
-!		end do
+		call aliasy_dobowe (fs,n,ft,r)
 	end if
 
 end subroutine
@@ -473,4 +399,4 @@ end subroutine
 
 
 
-end program res3
+end program res3bl
